faff61a2fc4917f6851154c46f419cfdbed6f173
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-01 22:20:24 -0700

    Add initial setup for PayNow NeoForge mod with Gradle configuration

---
+++ paynow-neoforge/src/main/java/gg/paynow/paynowneoforge/PayNowNeoForge.java
@@ -0,0 +1,41 @@
+package gg.paynow.paynowneoforge;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(PayNowNeoForge.MODID)
+public class PayNowNeoForge {
+    public static final String MODID = "paynow_neoforge";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public PayNowNeoForge(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
