6e904f5a2d85e88de8e2610ad577da09c9fb30a4
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-01 22:50:20 -0700

    Implement PayNow command registration and server event handling

--- paynow-neoforge/src/main/java/gg/paynow/paynowneoforge/PayNowNeoForge.java
+++ paynow-neoforge/src/main/java/gg/paynow/paynowneoforge/PayNowNeoForge.java
@@ -1,41 +1,134 @@
 package gg.paynow.paynowneoforge;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import gg.paynow.paynowlib.PayNowLib;
+import gg.paynow.paynowlib.PayNowUtils;
+import gg.paynow.paynowlib.events.PayNowEvent;
+import gg.paynow.paynowlib.events.PlayerJoinEventData;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
 
 @Mod(PayNowNeoForge.MODID)
 public class PayNowNeoForge {
     public static final String MODID = "paynow_neoforge";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public PayNowNeoForge(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+    private static PayNowNeoForge instance;
+    private MinecraftServer server;
+    private PayNowLib payNowLib;
 
+    private int lastCheck = 0;
+    private int lastEventsCheck = 0;
+
+    public PayNowNeoForge() {
+        instance = this;
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+    @SubscribeEvent
+    public void onCommandsRegistration(RegisterCommandsEvent event) {
+        PayNowNeoForgeCommand.register(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection(), instance);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        server = event.getServer();
+
+        String motd = server.getMotd();
+        this.payNowLib = new PayNowLib(command -> CompletableFuture.supplyAsync(() -> {
+            try {
+                server.getCommands().getDispatcher().execute(command, server.createCommandSourceStack());
+            } catch (CommandSyntaxException ignored) {}
+            return true; // Assume the command always succeeds, else it gets stuck.
+        }, server).join(), server.getLocalIp() + ":" + server.getPort(), Objects.equals(motd, "") ? "NeoForge Server" : motd);
+
+        this.payNowLib.setLogCallback((s, level) -> {
+            if (level == Level.SEVERE) {
+                LOGGER.error(s);
+            } else if(level == Level.WARNING) {
+                LOGGER.warn(s);
+            } else {
+                LOGGER.info(s);
+            }
+        });
+
+        this.payNowLib.loadPayNowConfig(this.getConfigFile());
+    }
+
+    @SubscribeEvent
+    public void onServerTick(ServerTickEvent.Post event) {
+        if(lastCheck > 0) {
+            lastCheck--;
+        } else {
+            lastCheck = this.payNowLib.getConfig().getApiCheckInterval() * 20;
+            this.check();
+        }
+
+        if(lastEventsCheck > 0) {
+            lastEventsCheck--;
+        } else {
+            lastEventsCheck = this.payNowLib.getConfig().getEventsQueueReportInterval() * 20;
+            this.payNowLib.reportEvents();
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity().level().isClientSide) return;
+        ServerPlayer player = event.getEntity() instanceof ServerPlayer ? (ServerPlayer) event.getEntity() : null;
+        if (player == null) return;
+        String ip = player.getIpAddress();
+        PayNowEvent payNowEvent = new PayNowEvent("player_join", new Date(), new PlayerJoinEventData(ip, player.getUUID()));
+        instance.getPayNowLib().registerEvent(payNowEvent);
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        server = null;
+        PayNowUtils.ASYNC_EXEC.shutdown();
+    }
+
+    private void check() {
+        List<String> onlinePlayersName = new ArrayList<>();
+        List<UUID> onlinePlayersUUID = new ArrayList<>();
+        for(ServerPlayer player : this.server.getPlayerList().getPlayers()) {
+            onlinePlayersName.add(player.getName().getString());
+            onlinePlayersUUID.add(player.getUUID());
+        }
+        payNowLib.fetchPendingCommands(onlinePlayersName, onlinePlayersUUID);
+    }
+
+    public void triggerConfigUpdate(){
+        this.payNowLib.savePayNowConfig(this.getConfigFile());
+        this.payNowLib.updateConfig();
+    }
+
+    private File getConfigFile() {
+        return new File(FMLPaths.CONFIGDIR.get().toFile(), "paynow.json");
+    }
+
+    public PayNowLib getPayNowLib() {
+        return payNowLib;
     }
 }
---
+++ paynow-neoforge/src/main/java/gg/paynow/paynowneoforge/PayNowNeoForgeCommand.java
@@ -0,0 +1,31 @@
+package gg.paynow.paynowneoforge;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
+
+public class PayNowNeoForgeCommand {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext registry, Commands.CommandSelection context, PayNowNeoForge mod) {
+        dispatcher.register(generateCommand(mod));
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> generateCommand(PayNowNeoForge mod) {
+        return Commands.literal("paynow")
+                .requires(source -> source.hasPermission(4))
+                .then(Commands.literal("link")
+                        .then(Commands.argument("token", string())
+                                .executes(context -> {
+                                    String token = context.getArgument("token", String.class);
+                                    mod.getPayNowLib().getConfig().setApiToken(token);
+                                    mod.triggerConfigUpdate();
+                                    context.getSource().sendSystemMessage(Component.literal("API token updated").withStyle(ChatFormatting.RED));
+                                    return 1;
+                                })));
+    }
+}
